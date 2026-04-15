f8d610f34da2ae6f1a1e12d21eb5cb2847c577e1
LenaYassserq <s202268180@kfupm.edu.sa>
2026-04-15 11:37:39 +0300

    Complete todo4

--- 7-2-restful-api/server/server.js
+++ 7-2-restful-api/server/server.js
@@ -20,6 +20,16 @@ app.get("/api/songs", async (req, res) => {
   res.json(rows);
 });
 
+app.get("/api/songs/:id", async (req, res) => {
+  const s = await Song.findById(req.params.id);
+
+  if (!s) {
+    return res.status(404).json({ message: "Song not found" });
+  }
+
+  res.json(s);
+});
+
 // api/songs (Insert song)
 app.post("/api/songs", async (req, res) => {
   try {
